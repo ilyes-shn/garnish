c663d2e1c961bee43d84f942687a955f0bec9c40
Ilyes Shn <infinityilyes@gmail.com>
2021-10-04 12:37:48 +0100

    underline

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -11,7 +11,7 @@ const [data, dispatch] = useData()
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat duis aute irure dolor in reprehenderit.
+        commodo consequat duis aute irure <span>dolor in reprehenderit.</span>
       </p>
       <button onClick={() => dispatch({type: 'cookie'})}>Accept</button>
     </div>
